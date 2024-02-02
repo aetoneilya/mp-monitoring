e343e4e80903aad61fe06055090871127c204db8
Ilya Udalov <udalov216@gmail.com>
2024-02-02 22:52:13 +0700

    init commit

---
+++ src/main/kotlin/com/machrist/mpmonitoring/MpMonitoringApplication.kt
@@ -0,0 +1,11 @@
+package com.machrist.mpmonitoring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MpMonitoringApplication
+
+fun main(args: Array<String>) {
+    runApplication<MpMonitoringApplication>(*args)
+}
---
+++ src/test/kotlin/com/machrist/mpmonitoring/MpMonitoringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.machrist.mpmonitoring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MpMonitoringApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
